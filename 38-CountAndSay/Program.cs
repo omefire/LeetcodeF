a15997db915ff9934ca792ab37f6dab811d11afe
Omar Mefire <omefire@gmail.com>
2018-09-04 06:43:45 -0700

    38 - Count And Say

---
+++ 38-CountAndSay/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_CountAndSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountAndSay(3));
+            Console.WriteLine(CountAndSay(4));
+            Console.WriteLine(CountAndSay(5));
+            Console.ReadLine();
+        }
+
+        static string CountAndSay(int n)
+        {
+            if(n <= 0)
+            {
+                throw new Exception("Invalid Argument");
+            }
+
+            if(n == 1)
+            {
+                return "1";
+            }
+
+            var prev = CountAndSay(n - 1);
+            int i = 0;
+            StringBuilder result = new StringBuilder();
+            while(i < prev.Length)
+            {
+                int count = 0;
+                int j = i;
+                while(j < prev.Length && (prev[i] == prev[j]))
+                {
+                    j++;
+                    count++;
+                }
+
+                result.Append(count);
+                result.Append(prev[i]);
+                i = j;
+            }
+
+            return result.ToString();
+        }
+    }
+}
