c5009af5356ff4ae3b624a83c82508868256f978
Omar Mefire <omefire@gmail.com>
2018-09-07 03:56:35 -0700

    88 - Merge Sorted Array

--- 88-MergeSortedArray/Program.cs
+++ 88-MergeSortedArray/Program.cs
@@ -10,11 +10,47 @@ namespace _88_MergeSortedArray
     {
         static void Main(string[] args)
         {
+            foreach(var e in MergeSortedArray(
+                new int[] {1, 2, 3, 0, 0, 0},
+                3,
+                new int[] {2, 5, 6},
+                3
+                ))
+            {
+                Console.Write(e + ", ");
+            }
+
+            Console.ReadLine();
         }
 
         static int[] MergeSortedArray(int[] nums1, int len1, int[] nums2, int len2)
         {
+            int k = nums1.Length - 1;
+            int i = len1 - 1;
+            int j = len2 - 1;
 
+            while((i >= 0) && (j >= 0))
+            {
+                if(nums1[i] >= nums2[j])
+                {
+                    nums1[k--] = nums1[i--];
+                } else
+                {
+                    nums1[k--] = nums2[j--];
+                }
+            }
+
+            while(i >= 0)
+            {
+                nums1[k--] = nums1[i--];
+            }
+
+            while(j >= 0)
+            {
+                nums1[k--] = nums2[j--];
+            }
+
+            return nums1;
         }
     }
 }
